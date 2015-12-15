d8cf5629d6faef292d0de58fc6a348220cc96b27
lmangeat <leon.mangeat@gmail.com>
2015-12-15 15:14:29 +0100

    Ajout connexion BDD mongolab

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ var bodyParser = require('body-parser');
 var swig = require('swig');
 var Cookies = require("cookies");
 var jwt = require('jsonwebtoken');
+var mongoose = require('mongoose');
 var config = require('./config.json');
 var Utils = require("./Utils.js");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
+
+console.log("Starting application");
+console.log("Loading Mongoose functionality");
+mongoose.set('debug', true);
+mongoose.connect("mongodb://root:root@ds027495.mongolab.com:27495/caveapi");
+mongoose.connection.on('error', function () {
+  console.log('Mongoose connection error');
+});
+mongoose.connection.once('open', function callback() {
+  console.log("Mongoose connected to the database");
+});
 
 var app = express();
 
@@ -72,16 +84,27 @@ if (app.get('env') === 'development') {
       message: err.message,
       error: err
     });
+    /*
+    res.json({
+      message: err.message,
+      status: err.status || 500,
+      stack: err.stack
+    });*/
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  /*
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
+  });*/
+  res.json({
+    message: err.message,
+    status: err.status || 500
   });
 });
 
