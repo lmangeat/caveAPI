770d5bce78fa3ab78d7a9df9ab061d43acde90e2
lmangeat <leon.mangeat@gmail.com>
2015-12-21 22:18:17 +0100

    Modification routes user et ajout README

---
+++ README.md
@@ -0,0 +1,5 @@
+Form response status:
+    - 601: "The two passwords doesn't match"
+    - 602: "The email address is not valid"
+    - 603: "The username is already used"
+    - 604: "The email address is already used"
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var sanitizer = require('sanitizer');
 
 var UserDb = require('../models/UserDB');
 var User = mongoose.model('User');
@@ -34,20 +35,62 @@ router.get('/getOne/email/:email', function(req, res, next) {
 });
 
 router.post('/create', function(req, res, next){
-    var user = new User(
-        {
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password
+    var regExEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+    var emailMatch;
+    var username = sanitizer.escape(req.body.username),
+        email = sanitizer.escape(req.body.email),
+        password = sanitizer.escape(req.body.password),
+        checkpassword = sanitizer.escape(req.body.checkpassword);
+
+    User.findOne({ $or:[ {username:username}, {email:email} ]})
+        .exec(function(err, user){
+            if(!user){
+                if(password == checkpassword){
+                    if( (emailMatch = email.match(regExEmail)) ){
+                        var user = new User(
+                            {
+                                username: username,
+                                email: email,
+                                password: password
+                            });
+                        user.save(function(err){
+                            if(err) {
+                                console.log(err);
+                                res.json({success: false});
+                            }
+                            else
+                                res.json({success: true});
+                        });
+                    }else{
+                        res.json({
+                            success: false,
+                            status: 602,
+                            message: "The email address is not valid"
+                        });
+                    }
+                }else{
+                    res.json({
+                        success: false,
+                        status: 601,
+                        message: "The two passwords doesn't match"
+                    });
+                }
+            }else{
+                if(user.username == username){
+                    res.json({
+                        success: false,
+                        status: 603,
+                        message: "The username is already used"
+                    });
+                }else if(user.email == email){
+                    res.json({
+                        success: false,
+                        status: 604,
+                        message: "The email address is already used"
+                    });
+                }
+            }
         });
-    user.save(function(err){
-        if(err) {
-            console.log(err);
-            res.json({success: false});
-        }
-        else
-            res.json({success: true});
-    });
 });
 
 router.delete('/delete/username/:username', function(req, res, next){
@@ -75,7 +118,7 @@ router.delete('/delete/id/:id', function(req, res, next){
 });
 
 router.put('/update', function(req, res, next){
-
+    
 });
 
 module.exports = router;
