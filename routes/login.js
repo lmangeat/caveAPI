1289919635cce2a567d1b6597005829ced496189
lmangeat <leon.mangeat@gmail.com>
2015-12-22 15:51:24 +0100

    Modification route login test

--- routes/login.js
+++ routes/login.js
@@ -11,6 +11,12 @@ var bcrypt = require("bcryptjs");
 
 var UserDB = require('../models/UserDB');
 var User = mongoose.model('User');
+var CaveDB = require('../models/CaveDB');
+var Cave = mongoose.model('Cave');
+var RowDB = require('../models/RowDB');
+var Row = mongoose.model('Row');
+var BottleDB = require('../models/BottleDB');
+var Bottle = mongoose.model('Bottle');
 
 router.post('/', function(req, res, next) {
     User.findOne({
@@ -31,14 +37,126 @@ router.post('/', function(req, res, next) {
 });
 
 router.get('/test', function(req, res, next) {
+    var bottle1Row1Cave1 = new Bottle({
+        vineyard: "Vineyard bottle 1 Row 1 Cave 1",
+        color: "Red",
+        type: "Type bottle 1 Row 1 Cave 1",
+        year: 1990,
+        conservation: 20,
+        name: "Name bottle 1 Row 1 Cave 1",
+        position: 1
+    });
+    var bottle2Row1Cave1 = new Bottle({
+        vineyard: "Vineyard bottle 2 Row 1 Cave 1",
+        color: "Red",
+        type: "Type bottle 2 Row 1 Cave 1",
+        year: 1992,
+        conservation: 20,
+        name: "Name bottle 2 Row 1 Cave 1",
+        position: 2
+    });
+    var bottle1Row2Cave1 = new Bottle({
+        vineyard: "Vineyard bottle 1 Row 2 Cave 1",
+        color: "White",
+        type: "Type bottle 1 Row 2 Cave 1",
+        year: 2005,
+        conservation: 15,
+        name: "Name bottle 1 Row 2 Cave 1",
+        position: 1
+    });
+    var bottle2Row2Cave1 = new Bottle({
+        vineyard: "Vineyard bottle 2 Row 2 Cave 1",
+        color: "White",
+        type: "Type bottle 2 Row 2 Cave 1",
+        year: 2003,
+        conservation: 15,
+        name: "Name bottle 2 Row 2 Cave 1",
+        position: 2
+    });
+    var bottle1Row1Cave2 = new Bottle({
+        vineyard: "Vineyard bottle 1 Row 1 Cave 2",
+        color: "Red",
+        type: "Type bottle 1 Row 1 Cave 2",
+        year: 1990,
+        conservation: 20,
+        name: "Name bottle 1 Row 1 Cave 2",
+        position: 1
+    });
+    var bottle2Row1Cave2 = new Bottle({
+        vineyard: "Vineyard bottle 2 Row 1 Cave 2",
+        color: "Red",
+        type: "Type bottle 2 Row 1 Cave 2",
+        year: 1992,
+        conservation: 20,
+        name: "Name bottle 2 Row 1 Cave 2",
+        position: 2
+    });
+    var bottle1Row2Cave2 = new Bottle({
+        vineyard: "Vineyard bottle 1 Row 2 Cave 2",
+        color: "White",
+        type: "Type bottle 1 Row 2 Cave 2",
+        year: 2005,
+        conservation: 15,
+        name: "Name bottle 1 Row 2 Cave 2",
+        position: 1
+    });
+    var bottle2Row2Cave2 = new Bottle({
+        vineyard: "Vineyard bottle 2 Row 2 Cave 2",
+        color: "White",
+        type: "Type bottle 2 Row 2 Cave 2",
+        year: 2003,
+        conservation: 15,
+        name: "Name bottle 2 Row 2 Cave 2",
+        position: 2
+    });
+    bottle1Row1Cave1.save();
+    bottle2Row1Cave1.save();
+    bottle1Row2Cave1.save();
+    bottle2Row2Cave1.save();
+    bottle1Row1Cave2.save();
+    bottle2Row1Cave2.save();
+    bottle1Row2Cave2.save();
+    bottle2Row2Cave2.save();
+
+    var row1Cave1 = Row({
+        bottles:[bottle1Row1Cave1, bottle2Row1Cave1],
+        nbMaxBottles: 20
+    });
+    var row2Cave1 = Row({
+        bottles:[bottle1Row2Cave1, bottle2Row2Cave1],
+        nbMaxBottles: 20
+    });
+    var row1Cave2 = Row({
+        bottles:[bottle1Row1Cave2, bottle2Row1Cave2],
+        nbMaxBottles: 20
+    });
+    var row2Cave2 = Row({
+        bottles:[bottle1Row2Cave2, bottle2Row2Cave2],
+        nbMaxBottles: 20
+    });
+    row1Cave1.save();
+    row2Cave1.save();
+    row1Cave2.save();
+    row2Cave2.save();
+
+    var cave1 = new Cave({
+        rows:[row1Cave1, row2Cave1]
+    });
+    var cave2 = new Cave({
+        rows:[row1Cave2, row2Cave2]
+    });
+    cave1.save();
+    cave2.save();
+
     var user = new User(
         {
             username: "test",
             email: "test@test.fr",
             password: "azerty",
-            caves: []
+            caves: [cave1, cave2]
         }
     );
+
     user.save();
     var token = jwt.sign(user, config.pwd.secret);
     new Cookies(req, res).set('token', token, {
